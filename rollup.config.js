b869c5d6739af817ccac71bec7934cd02a48a3fe
Elijah <ejsisgreat@gmail.com>
2019-12-18 18:28:44 -0600

    chore: dependency update

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,13 @@ import commonjs from 'rollup-plugin-commonjs';
 import typescript from 'rollup-plugin-typescript';
 import resolve from 'rollup-plugin-node-resolve';
 import pkg from './package.json';
-import execute from 'rollup-plugin-execute';
+import execute from 'rollup-plugin-command';
 
 const name = 'zip-tap';
 const sourcemap = true;
 const prod = process.env.NODE_ENV === 'production';
 const runningTests = process.env.NODE_ENV === 'test';
+const watching = process.env.ROLLUP_WATCH;
 
 const sharedOutputOptions = {
 	name,
@@ -24,7 +25,7 @@ export default {
 	plugins: [
 		resolve(),
 		commonjs(),
-		!prod && !runningTests && execute(`node ${pkg.main}`),
+		!prod && !runningTests && execute(`node ${pkg.main}`, { exitOnFail: !watching }),
 		typescript({
 			typescript: require('typescript'),
 		}),
